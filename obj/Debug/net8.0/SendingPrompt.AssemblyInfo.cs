db3750e7f086e230e40f58d88920ac6b94b788ee
Erdinç Karaman <erdokrmn@hotmail.com>
2025-05-20 13:06:42 +0300

    erdokrmn
    Gooogle colab kodlarını ekledim

--- obj/Debug/net8.0/SendingPrompt.AssemblyInfo.cs
+++ obj/Debug/net8.0/SendingPrompt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SendingPrompt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf902186ff061a830d3f3b8a39e69a8800d3413")]
 [assembly: System.Reflection.AssemblyProductAttribute("SendingPrompt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SendingPrompt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
